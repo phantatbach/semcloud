09f9fb4da1baa1d042ce057c0fb9365edadad922
Mariana Montes <mariana.montes@kuleuven.be>
2021-08-26 14:02:22 +0200

    add eucliMats

--- R/compute.R
+++ R/compute.R
@@ -89,7 +89,7 @@ procMats <- function(mat1, mat2,
   tokenlist <- row.names(mat1)[row.names(mat1) %in% row.names(mat2)]
   mat1b <- mat1[tokenlist, tokenlist]
   mat2b <- mat2[tokenlist, tokenlist]
-  result <- tryCatch(vegan::procrustes(mat1b, mat2b, symmetric = T)$ss, error = return)
+  result <- tryCatch(vegan::procrustes(mat1b, mat2b, symmetric = T)$ss, error = return())
   if (inherits(result, "error")) {
     message <- paste(mat1_name, mat2_name, transformed, Sys.time())
     write(message, "procrustes.log", append = T)
@@ -113,4 +113,22 @@ mantelMats <- function(mat1, mat2){
     mat2[tokenlist, tokenlist]
     )$statistic
   return(res)
+}
+
+#' Mean euclidean distance
+#'
+#' Computes the euclidean distance between the individual vectors
+#' and takes their mean.
+#'
+#' @param mat1 First matrix
+#' @param mat2 Second matrix
+#'
+#' @return Distance between matrix
+#' @export
+eucliMats <- function(mat1, mat2){
+  tokenlist <- row.names(mat1)[row.names(mat1) %in% row.names(mat2)]
+  (mat1[tokenlist, tokenlist]-mat2[tokenlist, tokenlist])^2 %>%
+    rowSums() %>%
+    sqrt() %>%
+    mean()
 }
